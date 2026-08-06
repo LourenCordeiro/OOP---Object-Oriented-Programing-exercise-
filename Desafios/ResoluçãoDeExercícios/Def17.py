527158747eb5c971fb54ad873b68e6430c124d70
lmcordeiroalves-alt <lmcordeiroalves.1994@gmail.com>
2026-08-06 17:24:51 -0300

    Exercício finalizado

--- Desafios/ResoluçãoDeExercícios/Def17.py
+++ Desafios/ResoluçãoDeExercícios/Def17.py
@@ -1,5 +1,8 @@
 from rich import print
-from rich.table import Table
+from rich.panel import Panel
+from rich.rule import Rule
+from rich.console import Group
+from rich.text import Text
 
 
 class Produto:
@@ -7,15 +10,14 @@ class Produto:
         self.nome = nome
         self.preco = preco
 
-
     def etiqueta(self):
-        tabela = Table(title='Produto')
-        tabela.add_column('nome')
-        tabela.add_column('preco')
-        tabela.add_row(self.nome, f"R$ {self.preco:,.2f}")
-        print(tabela)
-
-
+        conteudo = Group(
+            Text (self.nome, justify='center'), 
+            Rule (characters="-", style="white"),
+            Text(f"R${self.preco:,.2f}", justify='center'),
+        )
+        caixa = Panel(conteudo, title='Produto', style='white', width=40)
+        print(caixa)
 
 
 p1 = Produto('iPhone 17 Pro Max', 25_000.85)
