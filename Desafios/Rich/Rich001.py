5a58f4008a6895040bea6a69eae76c73615c2fa7
Louren Mayara <cordeiroalves.1994@outlook.com>
2026-08-07 21:53:14 -0300

    Implementação de verificação de números repetidos em lista aleatória

--- Desafios/Rich/Rich001.py
+++ Desafios/Rich/Rich001.py
@@ -1,4 +1,21 @@
-from rich import print
+#from rich import print
 
-print ("Olá [red]Mundo[/]! :earth_americas:")
-print ("Boa Noite!")
+#print ("Olá [red]Mundo[/]! :earth_americas:")
+#print ("Boa Noite!")
+
+import random
+nums = random.choices(range(10), k=4)
+print(nums)
+
+vistos = []
+tem_repetido = False
+
+for numero in nums:
+    if numero in vistos:
+        tem_repetido = True
+    vistos.append(numero)
+
+if tem_repetido:
+    print("True")
+else:
+    print("False")
